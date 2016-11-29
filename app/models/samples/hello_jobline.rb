2bea221941ad6b107023a79f88187a8f8b731fa1
Andre Joseph <ajcubeta@gmail.com>
2016-11-29 10:34:33 +0800

    Use jobline as sample

--- app/models/samples/hello_jobline.rb
+++ app/models/samples/hello_jobline.rb
@@ -3,5 +3,6 @@ require 'hexapdf'
 doc = HexaPDF::Document.new
 canvas = doc.pages.add_page.canvas
 canvas.font('Helvetica', size: 100)
-canvas.text("Hello World!", at: [20, 400])
-doc.write("hello_world.pdf", optimize: true)
+canvas.text("Hello Jobline Resources!", at: [20, 400])
+
+doc.write("hello_jobline.pdf", optimize: true)
