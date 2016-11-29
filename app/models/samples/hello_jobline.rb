be5c01feed187fc11fa944e9491fe8d744fbb6a8
Andre Joseph <ajcubeta@gmail.com>
2016-11-29 17:55:07 +0800

    Just testing some hexapdf samples

--- app/models/samples/hello_jobline.rb
+++ app/models/samples/hello_jobline.rb
@@ -1,8 +1,16 @@
 require 'hexapdf'
 
-doc = HexaPDF::Document.new
-canvas = doc.pages.add_page.canvas
-canvas.font('Helvetica', size: 100)
-canvas.text("Hello Jobline Resources!", at: [20, 400])
+# doc = HexaPDF::Document.new
+# canvas = doc.pages.add_page.canvas
+# canvas.font('Helvetica', size: 100)
+# canvas.text("Hello World!", at: [20, 400])
+#
+# doc.write("hello_world.pdf", optimize: true)
 
+# https://github.com/gettalong/hexapdf/blob/master/examples/hello_world.rb
+
+doc = HexaPDF::Document.new
+canvas = doc.pages.add.canvas
+canvas.font('Helvetica', size: 80)
+canvas.text("Hello Jobline!", at: [20, 400])
 doc.write("hello_jobline.pdf", optimize: true)
